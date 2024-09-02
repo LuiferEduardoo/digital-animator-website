15bad398c254d35f18cfa2fbbd64655f7e8623d8
Luifer <luifer01ortegaperez@gmail.com>
2024-09-02 15:18:23 -0500

    chore(api): agregar configuración del archivo .env

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import config from './config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -12,6 +15,19 @@ import { UsersModule } from './users/users.module';
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [AuthModule, DemonReelModule, ProjectsModule, MediaModule, ContactModule, EmailModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
+      load: [config],
+      isGlobal: true,
+    }),
+    AuthModule, 
+    DemonReelModule, 
+    ProjectsModule, 
+    MediaModule, 
+    ContactModule, 
+    EmailModule, 
+    UsersModule
+  ],
 })
 export class AppModule {}
