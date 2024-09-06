196ae32c759f600b38d4501671dd51213a863261
Luifer <luifer01ortegaperez@gmail.com>
2024-09-05 22:14:11 -0500

    feat(landing/i18n): agregar soporte para español e inglés en la página

---
+++ landing/src/i18n/ui.ts
@@ -0,0 +1,23 @@
+export const languages = {
+    en: "En",
+    es: "Es",
+};
+
+export const defaultLang = "es";
+
+export const showDefaultLang = false;
+
+export const ui = {
+    en: {
+        "nav.home": "Home",
+        "nav.portfolio": "Portfolio",
+        "nav.aboutMe": "About Me",
+        "footer.credit": "Designed & Developed by"
+    },
+    es: {
+        "nav.home": "Inicio",
+        "nav.portfolio": "Portafolio",
+        "nav.aboutMe": "Sobre mí",
+        "footer.credit": "Diseñado y desarrollador por"
+    }
+} as const;
