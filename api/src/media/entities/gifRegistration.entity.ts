58d2b6325ae451a798e9cfe318e9a2b607004135
Luifer <luifer01ortegaperez@gmail.com>
2024-09-11 21:08:40 -0500

    feat(api/media): crear entidad gifRegistration

---
+++ api/src/media/entities/gifRegistration.entity.ts
@@ -0,0 +1,44 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToOne,
+  JoinColumn,
+} from 'typeorm';
+import { FilesRegistration } from './filesRegistration.entity';
+
+@Entity('gif_registration')
+export class GifRegistration {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @OneToOne(() => FilesRegistration, { nullable: false })
+  @JoinColumn({ name: 'file_id' })
+  file: FilesRegistration;
+
+  @Column({ type: 'varchar', length: 32, nullable: false })
+  width: string;
+
+  @Column({ type: 'varchar', length: 32, nullable: false })
+  height: string;
+
+  @Column({ type: 'int', nullable: false })
+  number_frames: number;
+
+  @Column({ type: 'int', nullable: false })
+  duration_each_frame: number;
+
+  @CreateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  created_at: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  updated_at: Date;
+}
