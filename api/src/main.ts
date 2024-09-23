524d6bb1007536971b97b6240484fa6fffd957b9
Luifer <luifer01ortegaperez@gmail.com>
2024-09-23 15:34:24 -0500

    feat(validation): agregar configuración de ValidationPipe

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ClassSerializerInterceptor } from '@nestjs/common';
+import { ValidationPipe, ClassSerializerInterceptor } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
   await app.listen(process.env.APP_PORT);
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 }
